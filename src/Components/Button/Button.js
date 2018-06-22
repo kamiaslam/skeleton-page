cf71af60901a6d780a860734e698c5ab68914f9a
Kamran Aslam <Kamran@Kamrans-MBP.lan>
2018-06-22 19:02:12 +0100

    React project

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './btn.scss';
+
+class Button extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: "btn-warning"
+    }
+
+  }
+
+  addClass = e => {
+    
+    if (this.state.active && this.state.active === 'btn-warning') {
+      this.setState({
+        active: 'btn-danger'
+      })
+    }
+    else {
+        this.setState({
+            active: 'btn-warning'
+          })
+    }
+  }
+
+  render() {
+    return (
+        <div className="kamran">
+      <button className={'btn '+ this.state.active} onClick={(e)=>this.addClass(e)}>Sample button</button>
+      </div>
+    );
+  }
+}
+
+export default Button;
